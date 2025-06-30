c4786e16cc2ddd491069a57c057702a6efc05500
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-06-30 22:30:24 +0530

    Feat: Define game constants

--- src/game.c
+++ src/game.c
@@ -1,5 +1,22 @@
 #include "game.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <conio.h>
+#else
+#include <unistd.h>
+#include <termios.h>
+#include <fcntl.h>
+#endif
+
+// Game objects
+static int ball_x, ball_y;
+static int ball_dx, ball_dy;
+static int paddle1_y, paddle2_y;
+static int score1, score2;
 
 void game_init() {
     printf("Initializing game...\n");
--- src/game.h
+++ src/game.h
@@ -1,6 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
 
+// Game constants
+#define WIDTH 80
+#define HEIGHT 24
+#define PADDLE_HEIGHT 4
+
+// Function declarations
 void game_init();
 void game_run();
 void game_shutdown();
